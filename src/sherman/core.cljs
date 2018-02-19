ad20fd401d037953528ffda8903b8a4232b0e65a
Thomas Henderson <tom@mathpunk.net>
2018-02-19 13:21:42 -0600

    core: Begin hello-worlding a main function

---
+++ src/sherman/core.cljs
@@ -0,0 +1,6 @@
+(ns sherman.core
+  (:require [sherman.magic :as magic]
+            [sherman.grammar :as g]))
+
+
+(println "Hello, grammars!")
--- test/sherman/core_test.cljs
+++ test/sherman/core_test.cljs
@@ -1,8 +1,11 @@
 (ns sherman.core-test
-  (:require [cljs.test :refer-macros [deftest is testing async]]
+  (:require [sherman.core]
+            [cljs.test :refer-macros [deftest is testing async]]
             [cljs.spec.alpha :as s]))
+
 
 (deftest test-testing-in-core
   (is (= true true)))
+
 
 (cljs.test/run-tests)
