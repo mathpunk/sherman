e3fcb6da70dc0b2a8821a417289463790e0a6f5e
Thomas Henderson <tom@mathpunk.net>
2018-02-20 00:53:04 -0600

    Confirm 'app' is mutable

--- src/sherman/server.cljs
+++ src/sherman/server.cljs
@@ -3,20 +3,17 @@
             [sherman.magic :as magic]
             [cljs.nodejs :as nodejs]))
 
-                                        ; Bring in `express` and `http` modules.
 (defonce express (nodejs/require "express"))
 (defonce http (nodejs/require "http"))
 
                                         ; Create our app.
 (def app (express))
 
-                                        ; Mount GET handler.
 (. app (get "/magic/spell"
             (fn [req res]
               (. res
                  (send
                   (grammar/trace magic/magic-rules (rand-nth [ "spell" "item" ])))))))
-
 
 (. app (get "/magic/item"
             (fn [req res]
@@ -24,7 +21,10 @@
                  (send
                   (grammar/trace magic/magic-rules (rand-nth [ "spell" "item" ])))))))
 
+
                                         ; Listen on port 3000.
 (defn -main [& args]
-  (doto (.createServer http #(app %1 %2))
-    (.listen 3000)))
+  (let [port 3000]
+    (println (str "Serving magic on port " port))
+    (doto (.createServer http #(app %1 %2))
+      (.listen 3000))))
