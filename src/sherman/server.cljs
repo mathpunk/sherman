0fed018b92688d7e409d289b24a9b0a4d6adcb5c
Thomas Henderson <tom@mathpunk.net>
2018-02-19 23:38:11 -0600

    server (untested): Demonstrate serving grammar results via http
    This code is swiped from a "hello, httpserver" example showing what lumo can do.
    This should be mocked and tdd'd while I work out the rules of that interface.
    Some ideas for features include:
    
    - serve different grammars (selecting either on init, on req, or on either)
    - return edn, json, or html depending on req
    - accept modifications to the grammar

---
+++ src/sherman/server.cljs
@@ -0,0 +1,20 @@
+(ns sherman.server
+  (:require [sherman.grammar :as grammar]
+            [sherman.magic :as magic]
+            [cljs.nodejs :as nodejs]))
+
+                                        ; Bring in `express` and `http` modules.
+(defonce express (nodejs/require "express"))
+(defonce http (nodejs/require "http"))
+
+                                        ; Create our app.
+(def app (express))
+
+                                        ; Mount GET handler.
+(. app (get "/magic"
+            (fn [req res] (. res (send (grammar/trace magic/magic-rules (rand-nth [ "spell" "item" ])))))))
+
+                                        ; Listen on port 3000.
+(defn -main [& args] 
+  (doto (.createServer http #(app %1 %2))
+    (.listen 3000)))
